7bdc3c8d831ecf27d3cf201d2ef52405b0509576
tiltobias <61829955+tiltobias@users.noreply.github.com>
2020-12-11 17:20:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,13 +8,16 @@ let data = [
     {
         story: "You wake up.",
         choices: [
-            {description: "Leave", agility: 0.9, strength: 0, win: "you left", fail: "you got lost"},
-            {description: "Fight", agility: 0, strength: 0.9, win: "he dead now", fail: "you got clobberd"}
+            {description: "Leave", agility: 0.9, strength: 0, win: "you left", loss: "you got lost"},
+            {description: "Fight", agility: 0, strength: 0.9, win: "he dead now", loss: "you got clobberd"}
         ] 
     },
     {
-        story: "",
-        choices: []
+        story: "You see a tree",
+        choices: [
+            {description: "Climb", agility: 0.6, strength: 0.1, win: "you on top of tree", loss: "you fell down"},
+            {description: "Punch", agility: 0.1, strength: 0.6, win: "you punched the tree", loss: "tree punched back"}
+        ]
     },
     {
         story: "",
@@ -22,28 +25,77 @@ let data = [
     }
 ];
 
+let character = {
+    name: "Brynjolf",
+    sex: "male",
+    race: "viking",
+    agility: 100,
+    strength: 100,
+    inventory: ["chainmail", "potion"]
+};
+
+
+const personalize = function(content) {
+    let result = content;
+    result = result
+        .replace("-NAME-", character.name)
+        .replace("-RACE-", character.race)
+        .replace("-SEX-", character.sex)
+    ;
+    return result;
+};
+
+
 const scene = function(lvl) {
     let content = data[lvl].story;
-
+    btnCont.innerHTML = "";
+    
     for (i = 0; i < data[lvl].choices.length; i++) {
         const iData = data[lvl].choices[i];
-
+        
         let btn = document.createElement("button");
-        //btn.setAttribute("id", );
-        btn.setAttribute("value", iData.description);
-
+        btn.innerHTML = iData.description;
+        btn.setAttribute("data-location", "data["+ lvl +"].choices["+ i +"]");
         btnCont.appendChild(btn);
-
-
+        
         btn.addEventListener("click", makeChoice);
     };
 
     container.innerHTML = content;
 };
 
-const makeChoice = function() {
 
+const makeChoice = function() {
+    let iData = eval(this.getAttribute("data-location"));
+    let agility = +character.agility * +iData.agility;
+    let strength = +character.strength * +iData.strength;
+    
+    let content = "";
+    
+    if ((agility + strength) > Math.random()*100) {
+        console.log("win");
+        content = iData.win;
+    } else {
+        console.log("lose");
+        content = iData.loss;
+    };
+    
+    container.innerHTML = personalize(content);
+    
+    let nxt = document.createElement("button");
+    nxt.innerHTML = "Continue.";
+    
+    
 };
+
+
+
+
+
+
+
+
+
 
 
 
