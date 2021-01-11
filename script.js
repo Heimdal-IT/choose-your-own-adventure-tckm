42c650991bc5d1891aac350a8ae4a8e379773257
Chruusi <61830073+Chruusi@users.noreply.github.com>
2021-01-11 11:19:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let scenes = [
         ],
         background: "krig.jpg",
         audio: {
-            source: "",
+            source: "Slagmark - Theme.mp3",
             startTime: 0
         }
     },
@@ -27,19 +27,19 @@ let scenes = [
             {description: "Attack orc", agility: 0, strength: 0.9, win: "You charge towards the ginormous orc with your sword held high. He sees you covered with rage and might, and tries to punch you with his own curled up fists. His attack is nothing compared with yours. You then swing your sword with a fatal swift, and decapitate the orc.", loss: "You try to attack the orc, but as you go for the final blow, he suddenly gets his club loose and knocks you down. He then rises his mighty club up above his wart covered face and crushes you."},
             {description: "Sneak around orc", agility: 0.9, strength: 0, win: "You try to make a break for it while the orc is not paying attention to you. You crouch down behind some rocks and slowly but swiftly make your way around the orc. You then start to run away when you're far enough away from the orc.", loss: "You try to seize the opportunity as the orc is not paying attention. You crouch down to make yourself as tiny and unnoticeable as possible. You then slowly make your way around the orc, when suddenly you step on a tiny stick. You don’t even get the chance to react as the orc with a grunt smashes your skull with his mighty club."}
         ],
-        background: "Fire_effect_Energy_Animation_Background_Video_,_No_Copyright____Stock_Footage.gif",
+        background: "krig.jpg",
         audio: {
-            source: "",
+            source: "continue",
             startTime: 0
         }
     },
     {
         story: "You finally manage to escape the battlefield in order to find the map that leads to the enemy's leader. You take one last glance towards the battlefield before you run into what seems like an endless forest.-SPACE-As you travel along the path, you stumble across a split of the path that goes both left and right.-SPACE-Which path do you choose?",
         choices: [
-            {description: "Left", agility: 1, strength: 1, win: "You travel along the new path you chose. As you travel along, you stumble upon an abandoned wagon, inside you find …………….", loss: ""},
-            {description: "Right", agility: 1, strength: 1, win: "You travel along the new path you chose. As you travel along, you find a camp, but there is no one there, it seems abandoned. You take your time to search the camp for supplies or something handy to use, when you eventually find …………….", loss: ""}
+            {description: "Left", agility: 1, strength: 0, win: "You travel along the new path you chose. As you travel along, you stumble upon an abandoned wagon, inside you find …………….", loss: ""},
+            {description: "Right", agility: 1, strength: 0, win: "You travel along the new path you chose. As you travel along, you find a camp, but there is no one there, it seems abandoned. You take your time to search the camp for supplies or something handy to use, when you eventually find …………….", loss: ""}
         ],
-        background: "",
+        background: "skog.jfif",
         audio: {
             source: "",
             startTime: 0
@@ -49,8 +49,20 @@ let scenes = [
     {
         story: "You eventually find your way out of the forest, and you can now see an entrance to a cave further down the path. As you approach the entrance, you can feel a shiver down your spine, and you feel cold as the darkness of the cave starts to consume both you and all of the light around you. You follow some narrow stairs, you proceed cautiously as the ground is covered in some kind of goo that smells of dried blood.-SPACE-As you make yourself even further into the cave you hear a huge growl, but as you hear the growl you also make out some words at the same time. “I sense the smell of -pronoun---race-”, you hear as the sound of heavy footsteps approaching you keeps getting louder. “Show yourself -race-!” You slide into the shadows while being as quiet as you can. You now see a glimt of the creature that you now see is a huge mountain troll. You also spot the map your fellow comrade was talking about, hanging off his homesewn belt.",
         choices: [
+            {description: "Pickpocket", agility: 0.3, strength: 0, win: "You trail the troll as soon as he turns around. You then manage to get a grip on the map, that's hanging from some sort of hook on the troll’s belt. You slowly slide your hand towards the hook, and yank it off, without ripping the map itself.", loss: "You trail the troll as soon as he turns around. You then slowly make your way up behind it. As you’re almost up behind it, you suddenly slip on a wet rock and fall backwards, ripping the map off the hook as you fall. The troll then turns around, fumbling and confused on what just made that banging sound. The troll then spots you laying on the ground with your face facing it. It only takes the troll a second before it lets out a huge roar, and then clubs your whole body with one blow."},
+            {description: "Kill", agility: 0, strength: 0.7, win: "You run towards the troll as fast as you possibly manage. You then leap up on a boulder that stands beside it. You then let out a cry as you jump off the boulder sword drawn. The troll tries to face you, but it’s too late as you decapitate it.", loss: "You run towards the troll sword drawn as you let out your battlecry. The troll then turns towards you and starts to run towards you aswell. You’re both about to clash, but suddenly the troll trips on some rocks falling towards you. You try to leap out of its way, but it’s too late, crushing your body as it lands on you."}
+        ],
+        background: "cave.jfif",
+        audio: {
+            source: "Cave - Theme.mp3",
+            startTime: 0
+        }
+    },
+    {
+        story: "",
+        choices: [
             {description: "", agility: 0, strength: 0.9, win: "", loss: ""},
-            {description: "Sneak around orc", agility: 0.9, strength: 0, win: "", loss: ""}
+            {description: "", agility: 0.9, strength: 0, win: "", loss: ""}
         ],
         background: "",
         audio: {
@@ -62,19 +74,7 @@ let scenes = [
         story: "",
         choices: [
             {description: "", agility: 0, strength: 0.9, win: "", loss: ""},
-            {description: "Sneak around orc", agility: 0.9, strength: 0, win: "", loss: ""}
-        ],
-        background: "",
-        audio: {
-            source: "",
-            startTime: 0
-        }
-    },
-    {
-        story: "",
-        choices: [
-            {description: "", agility: 0, strength: 0.9, win: "", loss: ""},
-            {description: "Sneak around orc", agility: 0.9, strength: 0, win: "", loss: ""}
+            {description: "", agility: 0.9, strength: 0, win: "", loss: ""}
         ],
         background: "",
         audio: {
@@ -174,11 +174,15 @@ const scene = function(lvl) {
     };
 
     storyCont.innerHTML = personalize(content);
+
     background.style.backgroundImage = "url("+ scenes[lvl].background +")";
     
-    bgAudioSrc.setAttribute("src", scenes[lvl].audio.source);
-    bgAudio.load();
-    bgAudio.currentTime = scenes[lvl].audio.startTime;
+    if(scenes[lvl].audio.source !== "continue"){
+        bgAudioSrc.setAttribute("src", scenes[lvl].audio.source);
+        bgAudio.load();
+        bgAudio.currentTime = scenes[lvl].audio.startTime;
+    };
+    
 };
 
 
